7bc949c7265537b35893263bffa4ff945a93bfe7
Nathan Vijitbenjaronk <wvijitbe@purdue.edu>
2024-08-15 22:29:48 -0400

    added slide functionality for mostly functional basic 2048

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 
 #include <iostream>
 
+enum dir {
+  LEFT,
+  RIGHT,
+  UP,
+  DOWN
+};
+
 class Tile {
 public:
   int val;
@@ -24,6 +31,7 @@ void printBoard(Tile board[4][4]) {
     }
     printf("\n");
   }
+  printf("\n");
 }
 
 void spawnTile(Tile board[4][4], int val, int empty) {
@@ -40,10 +48,154 @@ void spawnTile(Tile board[4][4], int val, int empty) {
   }
 }
 
+// return val is if merge was done
+
+bool slideLeft(Tile board[4][4]) {
+  bool merged = false;
+  for (int i = 0; i < 4; i++) {
+    int merge_idx = 0; // index to merge with
+    for (int j = 1; j < 4; j++) {
+      if ((j == merge_idx) || (board[i][j].val == 0)) {
+        continue;
+      }
+      if (board[i][j].val == board[i][merge_idx].val) { // merge
+        board[i][merge_idx].val *= 2;
+        merge_idx++;
+        board[i][j].val = 0;
+        merged = true;
+      }
+      else {
+        if (board[i][merge_idx].val != 0) {
+          merge_idx++;
+        }
+        if (board[i][merge_idx].val == 0) { // slide left into empty slot
+          board[i][merge_idx].val = board[i][j].val;
+          board[i][j].val = 0;
+        }
+      }
+    }
+  }
+  return merged;
+}
+
+bool slideRight(Tile board[4][4]) {
+  bool merged = false;
+  for (int i = 0; i < 4; i++) {
+    int merge_idx = 4 - 1; // index to merge with
+    for (int j = (4 - 1) - 1; j >= 0; j--) {
+      if ((j == merge_idx) || (board[i][j].val == 0)) {
+        continue;
+      }
+      if (board[i][j].val == board[i][merge_idx].val) { // merge
+        board[i][merge_idx].val *= 2;
+        merge_idx--;
+        board[i][j].val = 0;
+        merged = true;
+      }
+      else {
+        if (board[i][merge_idx].val != 0) {
+          merge_idx--;
+        }
+        if (board[i][merge_idx].val == 0) { // slide left into empty slot
+          board[i][merge_idx].val = board[i][j].val;
+          board[i][j].val = 0;
+        }
+      }
+    }
+  }
+  return merged;
+}
+
+bool slideUp(Tile board[4][4]) {
+  bool merged = false;
+  for (int j = 0; j < 4; j++) {
+    int merge_idx = 0; // index to merge with
+    for (int i = 1; i < 4; i++) {
+      if ((i == merge_idx) || (board[i][j].val == 0)) {
+        continue;
+      }
+      if (board[i][j].val == board[merge_idx][j].val) { // merge
+        board[merge_idx][j].val *= 2;
+        merge_idx++;
+        board[i][j].val = 0;
+        merged = true;
+      }
+      else {
+        if (board[merge_idx][j].val != 0) {
+          merge_idx++;
+        }
+        if (board[merge_idx][j].val == 0) { // slide left into empty slot
+          board[merge_idx][j].val = board[i][j].val;
+          board[i][j].val = 0;
+        }
+      }
+    }
+  }
+  return merged;
+}
+
+bool slideDown(Tile board[4][4]) {
+  bool merged = false;
+  for (int j = 0; j < 4; j++) {
+    int merge_idx = 4 - 1; // index to merge with
+    for (int i = (4 - 1) - 1; i >= 0; i--) {
+      if ((i == merge_idx) || (board[i][j].val == 0)) {
+        continue;
+      }
+      if (board[i][j].val == board[merge_idx][j].val) { // merge
+        board[merge_idx][j].val *= 2;
+        merge_idx--;
+        board[i][j].val = 0;
+        merged = true;
+      }
+      else {
+        if (board[merge_idx][j].val != 0) {
+          merge_idx--;
+        }
+        if (board[merge_idx][j].val == 0) { // slide left into empty slot
+          board[merge_idx][j].val = board[i][j].val;
+          board[i][j].val = 0;
+        }
+      }
+    }
+  }
+  return merged;
+}
+
+
+
 int main() {
   srand(time(NULL));
   Tile board[4][4];
   //printBoard(board);
-  spawnTile(board, 5, 16);
-  printBoard(board);
+  int empty = 16;
+  char action;
+  while (true) {
+    spawnTile(board, 1, empty);
+    empty--;
+    printBoard(board);
+    std::cin >> action;
+    switch (action) {
+      case 'a':
+        if (slideLeft(board)) {
+          empty++;
+        }
+        break;
+      case 's':
+        if (slideDown(board)) {
+          empty++;
+        }
+        break;
+      case 'd':
+        if (slideRight(board)) {
+          empty++;
+        }
+        break;
+      case 'w':
+        if (slideUp(board)) {
+          empty++;
+        }
+        break;
+    }
+  }
 }
